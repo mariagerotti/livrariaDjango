ccfc40dea34572cab4d9ef3f27a0ac68db4a1c6b
mariagerotti <mariaeduarda.gerotti@gmail.com>
2023-04-04 16:52:21 -0300

    post usando rapdiAPI client, começando projeto com apis

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'livraria',
+    
 ]
 
 MIDDLEWARE = [
