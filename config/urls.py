3e619780957e601ba91072a86b81b9403694260d
mariagerotti <mariaeduarda.gerotti@gmail.com>
2023-04-25 16:13:56 -0300

    terminando de inserir a Editora

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from livraria.views import CategoriaViewSet
+from livraria.views import CategoriaViewSet, EditoraViewSet
 
 router = DefaultRouter()
 router.register(r"categorias", CategoriaViewSet)
+router.register(r"editoras", EditoraViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
