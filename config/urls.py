f137311cc56e4f8cc1627ca0f8a9ac3436432faf
mariagerotti <mariaeduarda.gerotti@gmail.com>
2023-04-25 16:10:01 -0300

    projeto formatado com black e isort

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path
-
 from rest_framework.routers import DefaultRouter
 
 from livraria.views import CategoriaViewSet
@@ -9,6 +8,6 @@ router = DefaultRouter()
 router.register(r"categorias", CategoriaViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include(router.urls)),
 ]
--- livraria/models.py
+++ livraria/models.py
@@ -15,6 +15,7 @@ class Editora(models.Model):
     def __str__(self):
         return self.nome
 
+
 class Autor(models.Model):
     nome = models.CharField(max_length=255)
     email = models.EmailField(null=True, blank=True)
@@ -22,9 +23,11 @@ class Autor(models.Model):
     def __str__(self):
         return f"{self.nome} - {self.email}"
 
+
 class Meta:
-    verbose_name_plural="Autores"
-    
+    verbose_name_plural = "Autores"
+
+
 class Autor(models.Model):
     nome = models.CharField(max_length=255)
     email = models.EmailField(null=True, blank=True)
@@ -34,23 +37,28 @@ class Autor(models.Model):
 
     class Meta:
         verbose_name_plural = "Autores"
-        
+
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
-    quantidade = models.IntegerField(default=0,  null=True, blank=True)
-    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    quantidade = models.IntegerField(default=0, null=True, blank=True)
+    preco = models.DecimalField(
+        max_digits=7, decimal_places=2, default=0, null=True, blank=True
+    )
     categoria = models.ForeignKey(
         Categoria, on_delete=models.PROTECT, related_name="livros"
     )
-    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros"
+    )
 
     def __str__(self):
         return f"{self.titulo} ({self.quantidade})"
-    
-#class Acessorio(models.Model):
- #   descricao = models.CharField(max_length=100)
-    
-  #  def __str__(self):
-   #     return self.descricao
+
+
+# class Acessorio(models.Model):
+#   descricao = models.CharField(max_length=100)
+
+#  def __str__(self):
+#     return self.descricao
