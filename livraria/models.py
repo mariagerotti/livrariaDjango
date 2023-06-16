010dada4c06f8063445da00c442e24cadf6d3006
mariagerotti <mariaeduarda.gerotti@gmail.com>
2023-06-16 16:39:03 -0300

    inserção many to many field ndo autor na classe Livro e ajuste ordem cód.

--- livraria/models.py
+++ livraria/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nome} - {self.email}"
+
+
+    class Meta:
+        verbose_name_plural = "Autores"
+
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
@@ -16,18 +27,6 @@ class Editora(models.Model):
         return self.nome
 
 
-class Autor(models.Model):
-    nome = models.CharField(max_length=255)
-    email = models.EmailField(null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.nome} - {self.email}"
-
-
-    class Meta:
-        verbose_name_plural = "Autores"
-
-
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
@@ -41,6 +40,7 @@ class Livro(models.Model):
     editora = models.ForeignKey(
         Editora, on_delete=models.PROTECT, related_name="livros"
     )
+    autor = models.ManyToManyField(Autor, related_name="livros")
 
     def __str__(self):
         return f"{self.titulo} ({self.quantidade})"
